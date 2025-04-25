311af07215158a195479442ca47f7a116ff4635e
Sotero-Romero <s225145@student.dtu.dk>
2025-04-25 13:05:33 +0200

    updating bayesian evaluatiion with new parameters

--- FinalVersion/Analysis/BayesianEvaluation.py
+++ FinalVersion/Analysis/BayesianEvaluation.py
@@ -1,12 +1,11 @@
 from FinalVersion.Analysis.ImageAnalysis import AnalyseImage
 
 
-def bayesianEvaluation(fidelity, mean_weight,mean_range,points,y=150,x=150):
+def bayesianEvaluation(canny_minimum, canny_maximum, canny_ksize,canny_sigma,gaussian_fidelity,gaussian_range,points,y=150,x=150):
     result=0
-    pore_cut_off = 3
 
     for type, original_image in points:
-        final_pores=AnalyseImage(original_image,mean_weight,mean_range,pore_cut_off,fidelity,overlay=False)
+        final_pores=AnalyseImage(original_image,canny_minimum, canny_maximum, canny_ksize,canny_sigma,gaussian_fidelity,gaussian_range,overlay=False)
         if final_pores[y,x]==200 and type=="background":
             result+=1
         elif final_pores[y,x]==0 and type=="pore":
